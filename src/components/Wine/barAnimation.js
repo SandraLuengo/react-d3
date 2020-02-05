a6de6c93270e88a0ca29b294ca3ffed38099fd2f
sandra <sandra.luengo@evoluciona.com>
2020-02-05 18:11:43 +0100

    all in files

---
+++ src/components/Wine/barAnimation.js
@@ -0,0 +1,53 @@
+import * as d3 from 'd3';
+
+const barAnimation = (dataset) => {
+    var margin = { top: 30, right: 30, bottom: 70, left: 60 },
+        width = 460 - margin.left - margin.right,
+        height = 400 - margin.top - margin.bottom;
+
+    // append the svg object to the body of the page
+    var svg = d3.select("#wine")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform",
+            "translate(" + margin.left + "," + margin.top + ")");
+
+    var x = d3.scaleBand()
+        .range([0, width])
+        .domain(dataset.children.map(d => d.Name))
+        .padding(0.2);
+    svg.append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x))
+        .selectAll("text")
+        .attr("transform", "translate(-10,0)rotate(-45)")
+        .style("text-anchor", "end");
+
+    var y = d3.scaleLinear()
+        .domain([0, d3.max(dataset.children, d => +d.Count)])
+        .range([height, 0]);
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    svg.selectAll("mybar")
+        .data(dataset.children)
+        .enter()
+        .append("rect")
+        .attr("x", d => x(d.Name))
+        .attr("y", d => y(0))
+        .attr("width", x.bandwidth())
+        .attr("height", d => height - y(0))
+        .attr("fill", "#69b3a2")
+    // Animation
+    svg.selectAll("rect")
+        .transition()
+        .duration(800)
+        .attr("y", d => y(d.Count))
+        .attr("height", d => height - y(d.Count))
+        .delay((d, i) => (i * 100))
+
+}
+
+export { barAnimation }
